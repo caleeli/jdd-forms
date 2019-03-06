249dcde6c25fb35ac821d1acf45b3ca993b317df
David Callizaya <davidcallizaya@gmail.com>
2019-03-06 19:00:42 -0400

    Fix default content

--- src/Models/Form.php
+++ src/Models/Form.php
@@ -13,11 +13,7 @@ class Form extends Model
 
     protected $dates = ['deleted_at'];
     protected $fillable = ['name', 'content', 'state'];
-    protected $attributes = ['name' => '', 'content' => [
-        '$' => [
-        ],
-        'JddContainer' => [],
-    ], 'state' => 'public'];
+    protected $attributes = ['name' => '', 'content' => '{"$":{},"JddContainer":[]}', 'state' => 'public'];
     protected $visible = ['name', 'content', 'state'];
     protected $casts = [
         'content' => 'array',
